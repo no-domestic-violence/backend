e687c5957843637f5dc2a1090a833b9cef933be0
Irina Baeva <irina.baeva@bonial.com>
2020-11-01 21:51:58 +0100

    added rules for linting and prettier

---
+++ server/src/routes/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  const token = req.header('auth-token');
+  if (!token) return res.status(401).send('Access denied');
+
+  // verifying token
+  try {
+    const verified = jwt.verify(token, 'SECRET_KEY');
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid token');
+  }
+};
