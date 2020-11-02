8f263ce6ee7421375a2df4aa71f2de5279f5fe95
soyoon choi <soyoon.choi@code.berlin>
2020-11-02 16:49:56 +0100

    added contacts into userSchema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,6 +4,16 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true },
+  contact_1: {
+    name: { type: String, default: '' },
+    phone: { type: String, default: '' },
+    message: { type: String, default: '' },
+  },
+  contact_2: {
+    name: { type: String, default: '' },
+    phone: { type: String, default: '' },
+    message: { type: String, default: '' },
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
