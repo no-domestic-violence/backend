73d23f0d4c39486e5661444db7acf9ed55760224
BehnazDer <behnaz6788@gmail.com>
2020-11-22 14:18:54 +0100

    name was corrected

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,40 +1,32 @@
 const express = require('express');
 const Article = require('../models/Article');
-const Articles = require('../models/Article');
+
 
 const router = express.Router();
 
 // Getting all the articles
 router.route('/articles').get((req, res) => {
-    console.log('All articles are shown');
-    Articles.find((err, foundArticles) => {
-      if (!err) {
-        return res.send(foundArticles);
-      }
-      return res.send(err);
-    });
+  console.log('All articles are shown');
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      return res.send(foundArticles);
+    }
+    return res.send(err);
   });
+});
 
 // Getting a specific article
 router.get('/articles/:id', function (req, res) {
-    console.log('One article is chosen');
-    Article.findById(req.params.id)
-    .exec(function(err, article){
-        if (err){
-            console.log(err);
-        }else {
-            res.send(article);
-        }
-        
-    });
+  console.log('One article is chosen');
+  Article.findById(req.params.id).exec(function (err, article) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(article);
+    }
+  });
 });
 
 // To Do : Bookmarking an article
 
 module.exports = router;
-
-
-
-
-
-
