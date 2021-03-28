e36a2032d5539549a939f8cf13b936499e65d9f5
Irina Baeva <irina.baeva@bonial.com>
2021-03-28 21:20:23 +0200

    Rename redis client data

--- src/controllers/hotline.controllers.js
+++ src/controllers/hotline.controllers.js
@@ -1,13 +1,14 @@
+import redis from 'redis';
 import Hotline from '../models/hotline.model';
 import Error from '../middleware/error/ErrorHandler';
-import redis from 'redis';
 
-//TODO: move it to initialisation file
+// TODO: move it to initialisation file
+// TODO: how to make it work on prod?
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const client = redis.createClient(REDIS_PORT);
 
-//log error to the console if any occurs
-client.on('error', err => {
+// log error to the console if any occurs
+client.on('error', (err) => {
   console.log(err);
 });
 
@@ -16,11 +17,11 @@ export const searchHotline = async (req, res, next) => {
   try {
     const querySearch = req.query.searchTerm;
 
-    client.get(querySearch, async (err, hotlinesResponse) => {
+    client.get(querySearch, async (err, hotlines) => {
       if (err) throw err;
 
-      if (hotlinesResponse) {
-        res.status(200).send(hotlinesResponse);
+      if (hotlines) {
+        res.status(200).send(hotlines);
       } else {
         const hotlinesResponse = await Hotline.find({
           $or: [
@@ -28,7 +29,7 @@ export const searchHotline = async (req, res, next) => {
             { organisation_name: { $regex: querySearch, $options: 'i' } },
           ],
         }).sort({ organisation_name: 1 });
-        client.setex(querySearch, 600, JSON.stringify(hotlinesResponse));
+        client.setex(querySearch, 3600, JSON.stringify(hotlinesResponse));
         res.status(200).send(hotlinesResponse);
       }
     });
