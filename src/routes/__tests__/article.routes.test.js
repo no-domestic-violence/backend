b42858fe0e8dd4fceceb3c31167fba008e6e99d2
soyoon choi <soyoon.choi@code.berlin>
2021-03-28 23:46:44 +0200

    mock authorization middlewares

--- src/routes/__tests__/article.routes.test.js
+++ src/routes/__tests__/article.routes.test.js
@@ -1,6 +1,27 @@
+jest.mock('../../middleware/verifyToken', () =>
+  jest.fn((req, res, next) => {
+    next();
+  }),
+);
+
+jest.mock('../../middleware/authorization', () => ({
+  ...jest.requireActual('../../middleware/authorization'),
+  checkCreateArticlePermission: jest.fn((req, res, next) => {
+    next();
+  }),
+  checkDeleteArticlePermission: jest.fn((req, res, next) => {
+    next();
+  }),
+}));
+
 import mongoose from 'mongoose';
 import request from 'supertest';
 import app from '../../app';
+import {
+  checkCreateArticlePermission,
+  checkDeleteArticlePermission,
+} from '../../middleware/authorization';
+import verifyToken from '../../middleware/verifyToken';
 import Article from '../../models/article.model';
 
 const articleData = {
@@ -13,14 +34,7 @@ const articleData = {
   created_at: new Date(),
 };
 
-jest.mock('../../middleware/verifyToken', () =>
-  jest.fn((req, res, next) => {
-    next();
-  }),
-);
-
 describe('Aricle endpoints', () => {
-
   beforeEach(async done => {
     await mongoose.connect(
       'mongodb://localhost:27017/test-db',
@@ -34,6 +48,7 @@ describe('Aricle endpoints', () => {
       mongoose.connection.close(() => done());
     });
   });
+
   test('should create a new article with POST request', async () => {
     await Article.create(articleData);
     const res = await request(app)
@@ -42,6 +57,8 @@ describe('Aricle endpoints', () => {
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('data');
     expect(res.body).toHaveProperty('success');
+    expect(verifyToken).toHaveBeenCalled();
+    expect(checkCreateArticlePermission).toHaveBeenCalled();
   });
   test('should get all articles as an array with GET request', async () => {
     await Article.create(articleData);
@@ -61,5 +78,7 @@ describe('Aricle endpoints', () => {
     let res = await request(app).delete('/api/articles/' + article.id);
     expect(res.statusCode).toEqual(202);
     expect(res.body.message).toEqual('Article was deleted!');
+    expect(verifyToken).toHaveBeenCalled();
+    expect(checkDeleteArticlePermission).toHaveBeenCalled();
   });
 });
