3068651b255ac5c610823f520c2a6fbd7f492afc
BehnazDer <behnaz6788@gmail.com>
2021-04-01 23:03:55 +0200

    merge conflicts resolved

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,8 @@
 /* eslint-disable  import/prefer-default-export */
 export const BASE_URI = '/api';
+
+export const ROLE = {
+  ADMIN: 'admin',
+  EDITOR: 'editor',
+  BASIC: 'basic',
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { ROLE } from '../constants';
 
 const contactSchema = new mongoose.Schema({
   name: { type: String },
@@ -22,5 +23,11 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 
   contacts: [contactSchema],
+  role: {
+    type: String,
+    required: true,
+    default: ROLE.BASIC,
+    enum: [ROLE.BASIC, ROLE.EDITOR, ROLE.ADMIN],
+  },
 });
 export default mongoose.model('User', userSchema);
