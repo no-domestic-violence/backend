011f91cd02f2916b709ff8be088a00261049bf19
Irina Baeva <irina.baeva@bonial.com>
2021-04-04 19:23:16 +0200

    Add prometheus

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -2,8 +2,9 @@ import redis from 'redis';
 import { REDIS_PORT } from '../constants';
 import logger from '../logger';
 
+/* eslint-disable  import/prefer-default-export */
 export const redisClient = redis.createClient(REDIS_PORT);
 
 redisClient.on('error', (e) => {
-  logger.log(e);
+  logger.error(e);
 });
