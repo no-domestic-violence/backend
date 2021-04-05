ec7698f32e4bd1fbacd64f246d5d4d28513689f8
soyoon choi <soyoon.choi@code.berlin>
2021-04-05 22:39:38 +0200

    connectdb beforeEach

--- src/routes/__tests__/article.routes.test.js
+++ src/routes/__tests__/article.routes.test.js
@@ -7,7 +7,11 @@ import {
 } from '../../middleware/authorization';
 import verifyToken from '../../middleware/verifyToken';
 import Article from '../../models/article.model';
-import { connectTestDb, closeTestDb, clearTestDb } from '../../test-setup/db-handler';
+import {
+  connectTestDb,
+  closeTestDb,
+  clearTestDb,
+} from '../../test-setup/db-handler';
 
 jest.mock('../../middleware/verifyToken', () =>
   jest.fn((req, res, next) => {
@@ -34,27 +38,12 @@ const articleData = {
   created_at: new Date(),
 };
 
-beforeAll(async() => await connectTestDb());
-
-afterEach(async() => await clearTestDb());
-
-afterAll(async() => await closeTestDb());
-
-
 describe('Aricle endpoints', () => {
-  // beforeEach(async done => {
-  //   await mongoose.connect(
-  //     'mongodb://localhost:27017/test-db',
-  //     { useNewUrlParser: true },
-  //     () => done(),
-  //   );
-  // });
+  beforeEach(async () => await connectTestDb());
 
-  // afterEach(async done => {
-  //   await mongoose.connection.db.dropDatabase(() => {
-  //     mongoose.connection.close(() => done());
-  //   });
-  // });
+  afterEach(async () => await clearTestDb());
+
+  afterAll(async () => await closeTestDb());
 
   test('should create a new article with POST request', async () => {
     await Article.create(articleData);
@@ -68,6 +57,7 @@ describe('Aricle endpoints', () => {
     expect(checkCreateArticlePermission).toHaveBeenCalled();
   });
   test('should get all articles as an array with GET request', async () => {
+    console.log('second test');
     await Article.create(articleData);
     const res = await request(app).get('/api/articles');
     expect(res.statusCode).toEqual(200);
--- src/test-setup/db-handler.js
+++ src/test-setup/db-handler.js
@@ -3,18 +3,16 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const mongod = new MongoMemoryServer();
-const opts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000
-};
 
-mongod.getInstanceInfo();
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 1000,
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const connectTestDb = async () => {
-    await mongoose.disconnect();
   const mongoUri = await mongod.getUri();
   await mongoose.connect(mongoUri, opts, err => {
     if (err) {
@@ -24,14 +22,15 @@ export const connectTestDb = async () => {
 };
 
 export const closeTestDb = async () => {
-    await mongoose.disconnect();
-    await mongod.stop();
-}
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
 
 export const clearTestDb = async () => {
-    const { collections } = mongoose.connection;
-    for (const key in collections){
-        await collections[key].deleteMany();
-    }
-}
-
+  const { collections } = mongoose.connection;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
