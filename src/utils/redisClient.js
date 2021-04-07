1ca94d33877a0445f1663e9ff92d8c55c4669575
soyoon choi <soyoon.choi@code.berlin>
2021-04-07 16:44:15 +0200

    change redisClient to default export

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -2,9 +2,9 @@ import redis from 'redis';
 import { REDIS_PORT } from '../constants';
 import logger from '../logger';
 
-/* eslint-disable  import/prefer-default-export */
-export const redisClient = redis.createClient(REDIS_PORT);
-
+const redisClient = redis.createClient(REDIS_PORT);
 redisClient.on('error', (e) => {
   logger.error(e);
 });
+
+export default redisClient;
