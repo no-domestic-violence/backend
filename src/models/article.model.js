3eba19ae00ca11c282dfeaae21a49aee5f511d8d
BehnazDer <behnaz6788@gmail.com>
2021-04-14 16:38:31 +0200

    small change in model

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -23,6 +23,7 @@ const articleSchema = new mongoose.Schema(
     violence_type: {
       type: [String],
       required: true,
+      validate: (v) => v == null || (v.length > 0),
     },
     created_at: {
       type: Date,
