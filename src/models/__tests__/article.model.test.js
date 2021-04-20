5c6c64ec82c2f358ac28c0a8a92ccda5a8849aac
BehnazDer <behnaz6788@gmail.com>
2021-04-20 10:23:23 +0200

    changed according to naming conventions

--- src/models/__tests__/article.model.test.js
+++ src/models/__tests__/article.model.test.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import {
-  normalArticle,
-  articleWithoutTitle,
-  articleWithoutAuthor,
-  articleWithoutText,
-  articleWithoutViolenceType,
-  articleWithoutUrlToImage,
-  articleWithoutCreatedAt
+  mockDefaultArticle,
+  mockArticleWithoutTitle,
+  mockArticleWithoutAuthor,
+  mockArticleWithoutText,
+  mockArticleWithoutViolenceType,
+  mockArticleWithoutUrlToImage,
+  mockArticleWithoutCreatedAt
 } from '../__mocks__/article';
 import articles from '../article.model';
 import {
@@ -27,42 +27,42 @@ afterAll(() => {
   closeDatabase();
 });
 
-describe('Normal article', () => {
+describe('Default article', () => {
   let article;
 
   beforeEach(async () => {
-    article = await articles.create(normalArticle);
+    article = await articles.create(mockDefaultArticle);
   });
 
   test('can be created correctly', async () => {
     expect(article).toBeTruthy();
-    expect(article.title).toBe(normalArticle.title);
-    expect(article.author).toBe(normalArticle.author);
-    expect(article.text).toBe(normalArticle.text);
-    expect(article.url_to_image).toBe(normalArticle.url_to_image);
+    expect(article.title).toBe(mockDefaultArticle.title);
+    expect(article.author).toBe(mockDefaultArticle.author);
+    expect(article.text).toBe(mockDefaultArticle.text);
+    expect(article.url_to_image).toBe(mockDefaultArticle.url_to_image);
     expect(article.violence_type).toContainEqual('emotional');
-    expect(Array.isArray(article.violence_type)).toBeTruthy();
+    expect(Array.isArray(mockDefaultArticle.violence_type)).toBeTruthy();
   });
 });
 
 describe('articles model', () => {
   test('should require title, author, text, url_to_image, violence_type and created_at ', async () => {
-    await expect(articles.create(articleWithoutTitle)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutTitle)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
-    await expect(articles.create(articleWithoutAuthor)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutAuthor)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
-    await expect(articles.create(articleWithoutText)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutText)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
-    await expect(articles.create(articleWithoutViolenceType)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutViolenceType)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
-    await expect(articles.create(articleWithoutUrlToImage)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutUrlToImage)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
-    await expect(articles.create(articleWithoutCreatedAt)).rejects.toThrow(
+    await expect(articles.create(mockArticleWithoutCreatedAt)).rejects.toThrow(
       mongoose.Error.ValidationError,
     );
   });
