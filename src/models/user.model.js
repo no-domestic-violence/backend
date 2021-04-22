74362d0cf89e372f94ba5c89d443931a209d1104
BehnazDer <behnaz6788@gmail.com>
2021-04-22 23:53:31 +0200

    fixing lint

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,16 +13,16 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
     unique: true,
-    sparse:true,
+    sparse: true,
   },
   email: {
     type: String,
     required: true,
     trim: true,
     unique: true,
-    sparse:true,
+    sparse: true,
   },
-  password: { type: String, required: true, sparse:true },
+  password: { type: String, required: true, sparse: true },
 
   contacts: [contactSchema],
   role: {
