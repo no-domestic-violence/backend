e7e1ff23a011647ea45024150c8c583de240a42f
BehnazDer <behnaz6788@gmail.com>
2021-04-22 23:23:40 +0200

    Refactoring login & changePassword endpoint & testing

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,62 +4,56 @@ import { generateToken } from '../utils/authentication';
 import User from '../models/user.model';
 import Error from '../middleware/error/ErrorHandler';
 
-//signup endpoint
+// signup endpoint
 export const validationErrors = async (req, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     next(
       Error.unprocessableEntity('Please provide a valid username or password'),
     );
-    return;
   }
-}
+};
 
-export const assertUserExists = async (email, next) =>  {
+export const assertUserExists = async (email, next) => {
   const userExist = await User.findOne({ email });
   if (userExist) {
-    next(
-      Error.badRequest('Email already exists'),
-    );
-    return;
-  }  
-}
+    next(Error.badRequest('Email already exists'));
+  }
+};
 
 export const createUser = async (username, email, password) => {
   const salt = await bcrypt.genSalt();
   const hashPassword = await bcrypt.hash(password, salt);
   const user = new User({
-    username: username,
-    email: email,
+    username,
+    email,
     password: hashPassword,
   });
-  return user
-}
+  return user;
+};
+
+export const requireAllfields = async (req, next) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    next(Error.badRequest('All fields are required'));
+  }
+};
 
 export const signup = async (req, res, next) => {
-  try {    
-    await validationErrors(req, next)
-    await assertUserExists(req.body.email, next)
+  try {
+    await validationErrors(req, next);
+    await assertUserExists(req.body.email, next);
     const user = await createUser(
       req.body.username,
       req.body.email,
       req.body.password,
-    )
-    
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      next(
-        Error.badRequest('All fields are required'),
-      );
-      return;
-    }
+    );
+    await requireAllfields(req, next);
     await user.save();
     // create and assign a token
     const token = generateToken(user);
-    res.status(201);
+    res.status(201).json({ user, success: true });
     res.send({
-      success: true,
-      user,
       token,
     });
   } catch (e) {
@@ -69,40 +63,28 @@ export const signup = async (req, res, next) => {
   }
 };
 
-//login endpoint
+// login endpoint
 export const getUser = async (email, next) => {
   const user = await User.findOne({ email });
   if (!user) {
-    next(
-      Error.notFound('User is not signed up'),
-    );
+    next(Error.notFound('User is not signed up'));
     return;
   }
-  return user
-}
+  return user;
+};
 
-export const validatePassword = async (password, next) => {
-  const validPassword = await bcrypt.compare(password, user.password);
+export const validatePassword = async (existingPassword, password, next) => {
+  const validPassword = await bcrypt.compare(password, existingPassword);
   if (!validPassword) {
-    next(
-      Error.unauthorized('Invalid Email or Password'),
-    );
-    return;
+    next(Error.unauthorized('Invalid Email or Password'));
   }
-}
-
+};
 
 export const login = async (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      next(
-        Error.unprocessableEntity('Please provide a valid username or password'),
-      );
-      return;
-    }
-    const user = await getUser(req.body.email, next)
-    await validatePassword(req.body.password, next)
+    await validationErrors(req, next);
+    const user = await getUser(req.body.email, next);
+    await validatePassword(user.password, req.body.password, next);
     const token = generateToken(user);
     res.header('auth-token', token).send({
       success: true,
@@ -114,40 +96,39 @@ export const login = async (req, res, next) => {
     next(e);
   }
 };
+// changePassword endpoint
+export const requiresAllfields = async (req, next) => {
+  const { email, oldPassword, password } = req.body;
+  if (!email || !oldPassword || !password) {
+    next(Error.unprocessableEntity('Please provide email and password'));
+  }
+};
 
-export const assertPasswordExist = async (email, next) => {
-  const user = await User.findOne({ email });
-  const isPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
-    if (!isPasswordCorrect) {
-      next(
-        Error.unauthorized('Old password is not correct'),
-      );
-      return;
-    }
-}
+export const assertPasswordExist = async (
+  oldPassword,
+  existingPassword,
+  next,
+) => {
+  const isPasswordCorrect = await bcrypt.compare(oldPassword, existingPassword);
+  if (!isPasswordCorrect) {
+    next(Error.unauthorized('Old password is not correct'));
+  }
+};
 
-
-
-export const createNewPassword = async (password) => {
+export const createNewPassword = async password => {
   const salt = await bcrypt.genSalt(10);
   const hashedNewPassword = await bcrypt.hash(password, salt);
-  const user = new User({
-    password: hashedNewPassword,
-  });
-  return user
-}
+
+  return hashedNewPassword;
+};
 
 export const changePassword = async (req, res, next) => {
   try {
-    const { email, oldPassword, password } = req.body;
-    if (!email || !oldPassword) {
-      next(
-        Error.unprocessableEntity('Please provide email and password'),
-      );
-      return;
-    }
-    await assertPasswordExist(req.body.email, next)
-    const user = await createNewPassword (req.body.password)
+    const user = await User.findOne({ email: req.body.email });
+    await requiresAllfields(req, next);
+    await assertPasswordExist(req.body.oldPassword, user.password, next);
+    const hashedNewPassword = await createNewPassword(req.body.password);
+    user.password = hashedNewPassword;
     await user.save();
     res.send('You updated the password');
   } catch (e) {
@@ -155,21 +136,19 @@ export const changePassword = async (req, res, next) => {
   }
 };
 
-//deleting account endpoint
+// deleting account endpoint
 export const getUserforDeletion = async (username, next) => {
   const user = await User.findOneAndDelete({ username });
   if (!user) {
-    next(
-      Error.notFound('User not found'),
-    );
+    next(Error.notFound('User not found'));
     return;
   }
-  return user
-}
+  return user;
+};
 
 export const deleteUser = async (req, res, next) => {
   try {
-    const user = await getUserforDeletion(req.query.username, next)   
+    const user = await getUserforDeletion(req.query.username, next);
     res.status(202).json({ user, message: 'User was deleted!' });
   } catch (e) {
     res.send(e);
