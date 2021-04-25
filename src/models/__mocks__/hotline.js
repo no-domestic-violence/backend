2b477c3ff3d564b8d06f0a415e278421e2b4780c
Irina Baeva <irina.baeva@bonial.com>
2021-04-25 22:17:59 +0200

    Clean up shelter test

--- src/models/__mocks__/hotline.js
+++ src/models/__mocks__/hotline.js
@@ -1,3 +1,5 @@
+/* eslint-disable  import/prefer-default-export */
+
 export const mockHotline = {
   organisation_name: 'Test name',
   country: 'Germany',
---
+++ src/models/__mocks__/shelter.js
@@ -0,0 +1,22 @@
+/* eslint-disable  import/prefer-default-export */
+
+export const mockShelters = [
+  {
+    place_name: 'Test name',
+    address: 'Berlin, Turmstrasse, 0',
+    contact_person: 'Test',
+    phone: '+497777777777',
+    longitude: 12.12,
+    latitude: 13.13,
+    description: 'Test description',
+  },
+  {
+    place_name: 'Test name2',
+    address: 'Berlin, Wicherstr, 10',
+    contact_person: 'Test contact',
+    phone: '+497777779999',
+    longitude: 12.12,
+    latitude: 13.13,
+    description: 'Test description',
+  },
+];
