555363ca81e124442cf0d0880d8a0d173dab2c63
BehnazDer <behnaz6788@gmail.com>
2021-04-29 14:47:56 +0200

    seperating the test cases

--- src/routes/__tests__/auth.routes.test.js
+++ src/routes/__tests__/auth.routes.test.js
@@ -10,89 +10,22 @@ import {
   clearDatabase,
 } from '../../utils/database';
 import { createUser } from '../../controllers/auth.controllers';
+import {
+  VALID_USERS,
+  INVALID_USERS,
+  MISSING_USER_INFO,
+  LOGIN_INVALID_USERS,
+  LOGIN_VALID_USERS,
+  INCORRECT_LOGIN_PASSWORD,
+  INCORRECT_CHANGEPASSWORD_INFO,
+  MISSING_CHANGEPASSWORD_INFO,
+} from '../__mocks__/auth.user';
 
 beforeAll(() => connectToDatabase());
 
 afterEach(() => clearDatabase());
 
 afterAll(() => closeDatabase());
-
-const getUserGenerator = () => {
-  let counter = 0;
-  return (overrides = {}) => {
-    counter += 1;
-    return {
-      username: `koosha${counter}`,
-      email: `kooshaala${counter}@gmail.com`,
-      password: 'Welcome123',
-      ...overrides,
-    };
-  };
-};
-const getUser = getUserGenerator();
-
-const VALID_USERS = {
-  DEFAULT: getUser(),
-  PASSWORD_WITHOUT_NUMBERS: getUser({ password: 'Welcometthere' }),
-  USERNAME_LENGTH_TWO: getUser({ username: 'hi' }),
-};
-
-const INVALID_USERS = {
-  INVALID_EMAIL: getUser({ email: 'jhhfuzegf' }),
-  USERNAME_LENGTH_1: getUser({ username: 'a' }),
-  INVALID_PASSWORD_LENGHT: getUser({ password: 'Welcome' }),
-};
-
-const MISSING_USER_INFO = {
-  DEFAULT: getUser({
-    username: undefined,
-    email: undefined,
-    password: undefined,
-  }),
-  MISSING_USERNAME: getUser({ username: '' }),
-  MISSING_PASSWORD: getUser({ password: '' }),
-  MISSING_EMAIL: getUser({ email: '' }),
-};
-
-const getLoginUser = (overrides = {}) => ({
-  email: 'hellothere@gmail.com',
-  password: 'hellothere123',
-  ...overrides,
-});
-
-const LOGIN_VALID_USERS = {
-  DEFAULT: getLoginUser(),
-  PASSWORD_WITHOUT_NUMBERS: getLoginUser({ password: 'Welcometthere' }),
-};
-
-const LOGIN_INVALID_USERS = {
-  DEFAULT: getLoginUser({ email: undefined, password: undefined }),
-  MISSING_EMAIL: getLoginUser({ email: undefined }),
-  EMPTY_EMAIL: getLoginUser({ email: '' }),
-  INVALID_EMAIL: getLoginUser({ email: 'jhhfuzegf' }),
-  INVALID_PASSWORD_LENGHT: getLoginUser({ password: 'Welcome' }),
-};
-
-const MISSING_LOGIN_USER_INFO = {
-  MISSING_PASSWORD: getLoginUser(),
-};
-
-const getchangePassword = (overrides = {}) => ({
-  email: 'welcome@gmail.com',
-  oldPassword: 'Welcome1',
-  password: 'Welcome2',
-  ...overrides,
-});
-
-const MISSING_PASSWORD_INFO = {
-  MISSING_OLDPASSWORD: getchangePassword({ oldPassword: '' }),
-  MISSING_PASSWORD: getchangePassword({ password: '' }),
-  MISSING_EMAIL: getchangePassword({ email: '' }),
-};
-
-const MISSING_CHANGEPASSWORD_INFO = {
-  MISSING_PASSWORD: getchangePassword(),
-};
 
 describe('signup endpoint', () => {
   describe('should save the user in database after signup', () => {
@@ -105,7 +38,7 @@ describe('signup endpoint', () => {
             .post('/api/signup')
             .send(payload);
           expect(res.statusCode).toEqual(201);
-          expect(res.body.message).toEqual('Signed up successfully !');
+          expect(res.body.message).toEqual('Signed up successfully!');
           expect(res.body).toHaveProperty('success');
           expect(res.body).toHaveProperty('user');
 
@@ -118,7 +51,7 @@ describe('signup endpoint', () => {
     });
   });
 
-  describe('fails with invalid data', () => {
+  describe('signup fails with invalid data', () => {
     Object.entries(INVALID_USERS).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
@@ -135,7 +68,7 @@ describe('signup endpoint', () => {
     });
   });
 
-  describe('fails with missing user data', () => {
+  describe('signup fails with missing user data', () => {
     Object.entries(MISSING_USER_INFO).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
@@ -150,7 +83,7 @@ describe('signup endpoint', () => {
     });
   });
 
-  describe('fails with already signed up user', () => {
+  describe('signup fails with already signed up user', () => {
     Object.entries(VALID_USERS).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
@@ -200,14 +133,14 @@ describe('login endpoint', () => {
             .post('/api/login')
             .send(payload);
           expect(res.statusCode).toEqual(200);
-          expect(res.body.message).toEqual('Logged in successfully !');
+          expect(res.body.message).toEqual('Logged in successfully!');
         });
       });
     });
   });
 
-  describe('fails with invalid password', () => {
-    Object.entries(MISSING_LOGIN_USER_INFO).forEach(testCase => {
+  describe('login fails with invalid password', () => {
+    Object.entries(INCORRECT_LOGIN_PASSWORD).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
         let user;
@@ -233,7 +166,7 @@ describe('login endpoint', () => {
     });
   });
 
-  describe('fails with invalid data', () => {
+  describe('login fails with invalid data', () => {
     Object.entries(LOGIN_INVALID_USERS).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
@@ -250,7 +183,7 @@ describe('login endpoint', () => {
     });
   });
 
-  describe('fails with not signed up user', () => {
+  describe('login fails with not signed up user', () => {
     Object.entries(LOGIN_VALID_USERS).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
@@ -310,8 +243,8 @@ describe('changePassword endpoint', () => {
     });
   });
 
-  describe('fails because the old password is incorrect', () => {
-    Object.entries(MISSING_CHANGEPASSWORD_INFO).forEach(testCase => {
+  describe('change password fails because the old password is incorrect', () => {
+    Object.entries(INCORRECT_CHANGEPASSWORD_INFO).forEach(testCase => {
       const [key, userData] = testCase;
       const payload = {
         email: userData.email,
@@ -340,8 +273,8 @@ describe('changePassword endpoint', () => {
     });
   });
 
-  describe('fails with missing user email,password & oldPassword', () => {
-    Object.entries(MISSING_PASSWORD_INFO).forEach(testCase => {
+  describe('change password fails with missing user email,password & oldPassword', () => {
+    Object.entries(MISSING_CHANGEPASSWORD_INFO).forEach(testCase => {
       const [key, payload] = testCase;
       describe(key, () => {
         test(key, async () => {
