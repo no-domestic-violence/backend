c59dcb31a3cf447aa5138886c52f84bcf224a997
BehnazDer <behnaz6788@gmail.com>
2021-04-29 14:57:02 +0200

    adding controllers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,13 +6,19 @@ import {
   login,
   changePassword,
   deleteUser,
+  requireAllfields,
+  validationErrors,
+  // eslint-disable-next-line import/named
+  requireCredentials,
 } from '../controllers';
 
 const router = express.Router();
 
-router.route('/signup').post(signupValidation, signup);
-router.route('/login').post(loginValidation, login);
-router.route('/changePassword').post(changePassword);
+router
+  .route('/signup')
+  .post(signupValidation, requireAllfields, validationErrors, signup);
+router.route('/login').post(loginValidation, validationErrors, login);
+router.route('/changePassword').post(requireCredentials, changePassword);
 router.route('/deleteUser').delete(deleteUser);
 
 export default router;
