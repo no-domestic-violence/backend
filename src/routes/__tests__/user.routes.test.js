5231e10cae50a26458e3b67f83b5b1b292e3c6cd
soyoon choi <soyoon.choi@code.berlin>
2021-05-03 16:22:23 +0200

    add check token process when testing user routes

--- src/routes/__tests__/user.routes.test.js
+++ src/routes/__tests__/user.routes.test.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import request from 'supertest';
 import app from '../../app';
 import User from '../../models/user.model';
@@ -7,12 +8,16 @@ import {
   clearDatabase,
 } from '../../utils/database';
 import { mockUserWithContact, mockContact } from '../../models/__mocks__/user';
-import { verifyToken } from '../../middleware';
 
-jest.mock('../../middleware/verifyToken', () =>
-  jest.fn((req, res, next) => {
-    next();
-  }),
+const mockToken = jwt.sign(
+  {
+    _id: mockUserWithContact._id,
+    email: mockUserWithContact.email,
+    username: mockUserWithContact.username,
+    role: mockUserWithContact.role,
+  },
+  process.env.JWT_SECRET,
+  { expiresIn: '1m' },
 );
 
 beforeAll(() => {
@@ -41,6 +46,7 @@ describe('User endpoints', () => {
   test('should get all contacts of a user with GET request', async () => {
     await request(app)
       .get(`/api/users/${username}/contacts`)
+      .set({ 'auth-token': mockToken })
       .expect(200)
       .then(res => {
         expect(res.body.contacts[0].name).toBe(createdUser.contacts[0].name);
@@ -51,20 +57,23 @@ describe('User endpoints', () => {
       });
   });
 
-  test('should call verifyToken middleware with GET request', async () => {
-    await request(app).get(`/api/users/${username}/contacts`);
-    expect(verifyToken).toBeCalledTimes(1);
+  test('should respond with an error when getting contacts without auth token', async () => {
+    await request(app)
+      .get(`/api/users/${username}/contacts`)
+      .expect(401);
   });
 
   test('should respond with an error when getting contacts of nonexistent user', async () => {
     await request(app)
       .get(`/api/users/invalidusername/contacts`)
+      .set({ 'auth-token': mockToken })
       .expect(404);
   });
 
   test('should throw bad request error if required field for a contact is missing when adding or editing the contact', async () => {
     await request(app)
       .patch(`/api/users/${username}/contacts`)
+      .set({ 'auth-token': mockToken })
       .send({})
       .expect(400);
   });
@@ -72,6 +81,7 @@ describe('User endpoints', () => {
   test('should successfully add a contact', async () => {
     await request(app)
       .patch(`/api/users/${username}/contacts`)
+      .set({ 'auth-token': mockToken })
       .send(mockContact)
       .expect(201)
       .then(res => {
@@ -82,14 +92,16 @@ describe('User endpoints', () => {
       });
   });
 
-  test('should call verifyToken middleware when adding a contact', async () => {
-    await request(app).patch(`/api/users/${username}/contacts`);
-    expect(verifyToken).toBeCalledTimes(1);
+  test('should respond with error when adding contact without auth token', async () => {
+    await request(app)
+      .patch(`/api/users/${username}/contacts`)
+      .send(mockContact)
+      .expect(401);
   });
-
   test('should respond with an error when adding a contact on nonexistent user', async () => {
     await request(app)
       .patch(`/api/users/invalidusername/contacts`)
+      .set({ 'auth-token': mockToken })
       .send(mockContact)
       .expect(404);
   });
@@ -97,6 +109,7 @@ describe('User endpoints', () => {
   test('should successfully edit a contact', async () => {
     await request(app)
       .patch(`/api/users/${username}/contacts/${contactId}`)
+      .set({ 'auth-token': mockToken })
       .send(mockContact)
       .expect(201)
       .then(res => {
@@ -106,15 +119,18 @@ describe('User endpoints', () => {
       });
   });
 
-  test('should call verifyToken middleware when editing a contact', async () => {
-    await request(app).patch(`/api/users/${username}/contacts/${contactId}`);
-    expect(verifyToken).toBeCalledTimes(1);
+  test('should respond with error when editing contact without auth token', async () => {
+    await request(app)
+      .patch(`/api/users/${username}/contacts/${contactId}`)
+      .send(mockContact)
+      .expect(401);
   });
 
   test('should respond with an error when editing not existing contact', async () => {
     const invalidContactId = '6062e6501e80a94test40522';
-    const res = await request(app)
+    await request(app)
       .patch(`/api/users/${username}/contacts/${invalidContactId}`)
+      .set({ 'auth-token': mockToken })
       .send(mockContact)
       .expect(404);
   });
@@ -122,18 +138,22 @@ describe('User endpoints', () => {
   test('should successfully delete contact', async () => {
     await request(app)
       .delete(`/api/users/${username}/contacts/${contactId}`)
+      .set({ 'auth-token': mockToken })
       .expect(202);
   });
 
-  test('should call verifyToken middleware when deleting a contact', async () => {
-    await request(app).delete(`/api/users/${username}/contacts/${contactId}`);
-    expect(verifyToken).toBeCalledTimes(1);
+  test('should respond with error when deleting contact without auth token', async () => {
+    await request(app)
+      .patch(`/api/users/${username}/contacts/${contactId}`)
+      .send(mockContact)
+      .expect(401);
   });
 
   test('should respond with an error when deleting not existing contact', async () => {
     const invalidContactId = '6062e6501e80a94test40522';
     await request(app)
       .delete(`/api/users/${username}/contacts/${invalidContactId}`)
+      .set({ 'auth-token': mockToken })
       .expect(404);
   });
 });
