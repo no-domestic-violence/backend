4158c7c4f43dd0303750ddea3a3790a525c95328
Irina Baeva <irina.baeva@bonial.com>
2021-05-13 13:33:59 +0200

    [refactoring] remove duplicated code with extract function getCache

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -3,14 +3,27 @@ import { REDIS_PORT } from '../constants';
 import logger from '../logger';
 
 const redisClient = redis.createClient(REDIS_PORT);
-redisClient.on('error', (e) => {
+redisClient.on('error', e => {
   logger.error(e);
 });
 
-const setRedisCache = (name, data) => {
-  return process.env.NODE_ENV === 'development' &&
-      redisClient.setex(name, 3600, JSON.stringify(data));
-}
+const setRedisCache = (keyName, data) => {
+  return (
+    process.env.NODE_ENV === 'development' &&
+    redisClient.setex(keyName, 3600, JSON.stringify(data))
+  );
+};
 
-export {redisClient, setRedisCache};
+const getRedisCache = (keyName, serverResponse, next) => {
+  process.env.NODE_ENV === 'production' && next();
+  redisClient.get(keyName, (err, data) => {
+    if (err) throw err;
+    if (data) {
+      serverResponse.status(200).send(data);
+    } else {
+      next();
+    }
+  });
+};
 
+export { redisClient, setRedisCache, getRedisCache };
