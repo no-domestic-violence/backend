63f145d22839e65b44a00f0b8f9a548214488138
soyoon choi <soyoon.choi@code.berlin>
2021-05-21 14:52:58 +0200

    change permission to only admins and editors for article

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -2,19 +2,15 @@
 /* eslint-disable import/prefer-default-export */
 import { ROLE } from '../constants';
 
-const { BASIC, EDITOR, ADMIN } = ROLE;
+const { EDITOR, ADMIN } = ROLE;
 
 export const hasDeleteArticlePermission = (user, authorId) => {
   return user.role === ADMIN || user._id === authorId.toString();
 };
 
 // remove permission from basic when admin,editor assign is implemented
-export const hasCreateArticlePermission = (user) => {
-  return (
-    user.role === BASIC
-    || user.role === ADMIN
-    || user.role === EDITOR
-  );
+export const hasCreateArticlePermission = user => {
+  return user.role === ADMIN || user.role === EDITOR;
 };
 
 export const hasEditArticlePermission = (user, authorId) => {
