1a20af9bcb27a4ad2c75b9fef041dc61a5229f85
BehnazDer <behnaz6788@gmail.com>
2021-07-27 23:37:37 +0200

    implementing ratelimiting to prevent Brute-force-attack

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,15 +7,47 @@ import {
 } from '../middleware/authMiddlewares';
 
 // eslint-disable-next-line object-curly-newline
-import { signup, login, changePassword, deleteUser } from '../controllers';
+import {
+  signup,
+  login,
+  changePassword,
+  deleteUser,
+  refreshUserToken,
+} from '../controllers';
+import { verifyToken } from '../middleware';
+import {
+  GeneralRouteRateLimit,
+  loginEmailRouteRateLimit,
+  loginIpRouteRateLimit,
+  loginIpEmailRouteRateLimit,
+} from '../controllers/rateLimitControllers';
 
 const router = express.Router();
 
 router
   .route('/signup')
-  .post(signupValidation, requireAllfields, validationErrors, signup);
-router.route('/login').post(loginValidation, validationErrors, login);
-router.route('/changePassword').post(requireCredentials, changePassword);
+  .post(
+    GeneralRouteRateLimit('signup'),
+    signupValidation,
+    requireAllfields,
+    validationErrors,
+    signup,
+  );
+router
+  .route('/login')
+  .post(
+    GeneralRouteRateLimit('login'),
+    loginEmailRouteRateLimit,
+    loginIpRouteRateLimit,
+    loginIpEmailRouteRateLimit,
+    loginValidation,
+    validationErrors,
+    login,
+  );
+router
+  .route('/changePassword')
+  .post(verifyToken, requireCredentials, changePassword);
 router.route('/deleteUser').delete(deleteUser);
+router.route('/refreshToken').post(verifyToken, refreshUserToken);
 
 export default router;
