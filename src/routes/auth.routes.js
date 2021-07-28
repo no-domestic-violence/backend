cc2e802a748e8169044f758823e219272af620d5
Soyoon Choi <soyoon.choi@code.berlin>
2021-07-29 01:06:37 +0200

    add refreshToken and logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { signupValidation, loginValidation } from '../utils/authentication';
-import {
-  validationErrors,
+// import {
+//   validationErrors,
+//   requireAllfields,
+//   requireCredentials,
+// } from '../middleware/authMiddlewares';
+import { verifyAccessToken, validationErrors,
   requireAllfields,
-  requireCredentials,
-} from '../middleware/authMiddlewares';
-
+  requireCredentials, } from '../middleware'
 // eslint-disable-next-line object-curly-newline
-import { signup, login, changePassword, deleteUser } from '../controllers';
+import { signup, login, changePassword, deleteUser, verifyRefreshToken, logout } from '../controllers';
 
 const router = express.Router();
 
@@ -15,7 +17,9 @@ router
   .route('/signup')
   .post(signupValidation, requireAllfields, validationErrors, signup);
 router.route('/login').post(loginValidation, validationErrors, login);
-router.route('/changePassword').post(requireCredentials, changePassword);
-router.route('/deleteUser').delete(deleteUser);
+router.route('/logout').post(logout);
+router.route('/refreshToken').post(verifyRefreshToken);
+router.route('/changePassword').post(verifyAccessToken, requireCredentials, changePassword);
+router.route('/deleteUser').delete(verifyAccessToken, deleteUser);
 
 export default router;
