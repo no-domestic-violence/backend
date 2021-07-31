d8abc0c0e2bfabdb1d40de45875b06fccfbed9b2
BehnazDer <behnaz6788@gmail.com>
2021-07-31 13:00:45 +0200

    captcha in BE

---
+++ src/utils/captcha.js
@@ -0,0 +1,22 @@
+import * as fetch from 'node-fetch';
+
+const verifyCaptcha = async captchaResp => {
+  try {
+    const captchaSecret = process.env.CAPTCHA_SECRET_KEY || '';
+    const resp = await fetch(
+      'https://www.google.com/recaptcha/api/siteverify',
+      {
+        data: JSON.stringify({
+          secret: captchaSecret,
+          response: captchaResp,
+        }),
+      },
+    );
+    return resp.success;
+  } catch (err) {
+    console.log(`Error while verifzing captcha: `, err.message);
+    return false;
+  }
+};
+
+export { verifyCaptcha };
