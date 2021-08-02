6b5a4c09801670a466396fa7e46b92050c35f832
BehnazDer <behnaz6788@gmail.com>
2021-08-03 01:00:06 +0200

    Captcha updates

--- src/index.js
+++ src/index.js
@@ -7,24 +7,31 @@ import https from 'https';
 import fs from 'fs';
 dotenv.config();
 
-let https_server = https.createServer({
-  cert: fs.readFileSync('./certificate.crt'),
-  key: fs.readFileSync('./private.key'),
-  passphrase: process.env.KEY_PASSPHRASE
-}, app);
-
 const startServer = async () => {
   try {
     await connectToDatabase();
     app.listen(HTTP_PORT, '0.0.0.0', () => {
       /* eslint-disable no-console */
-      logger.info(`Http server is running on http://localhost:${HTTP_PORT}/api`);
+      logger.info(
+        `Http server is running on http://localhost:${HTTP_PORT}/api`,
+      );
     });
     // https server running in dev mode
     if (process.env.NODE_ENV === 'development') {
+      const https_server = https.createServer(
+        {
+          cert: fs.readFileSync('./certificate.crt'),
+          key: fs.readFileSync('./private.key'),
+          passphrase: process.env.KEY_PASSPHRASE,
+        },
+        app,
+      );
+
       https_server.listen(HTTPS_PORT, () => {
         /* eslint-disable no-console */
-        logger.info(`Https server is running on https://localhost:${HTTPS_PORT}/api`);
+        logger.info(
+          `Https server is running on https://localhost:${HTTPS_PORT}/api`,
+        );
       });
     }
   } catch (e) {
